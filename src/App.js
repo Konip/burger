8b26e2de39e00dad55db810414b79f87fb57441a
Konip <gunpowderbit@gmail.com>
2021-02-04 14:47:27 +0300

    first com

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
+import BurgerBlock from './components/BurgerBlock';
+import Button from './components/Button';
+import Header from './components/Header';
+import db from "./db.json"
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header>
+        <Button />
+      </Header>
+      {
+        db.map(d => (
+          <BurgerBlock props={d[0]} />
+        ))
+      }
+
     </div>
   );
 }
